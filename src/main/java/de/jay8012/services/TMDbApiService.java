52ba22b43f8b35f4c6a1e700b9212e3e912ff21d
Jay8012 <sanjay.hartmann.12@gmail.com>
2024-04-25 20:43:35 +0200

    http client angefangen für die suche von filmen

---
+++ src/main/java/de/jay8012/services/TMDbApiService.java
@@ -0,0 +1,37 @@
+package de.jay8012.services;
+
+import de.jay8012.exceptions.NotImplementedException;
+import de.jay8012.utils.PropertiesUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Properties;
+
+public class TMDbApiService {
+    private Properties properties;
+    public TMDbApiService() {
+        properties = PropertiesUtils.getProperties();
+    }
+
+    public void getMetadataForMovie(String movieName) throws IOException, InterruptedException {
+        if (properties == null) {
+            throw new NotImplementedException();
+        }
+        String baseurl = properties.getProperty("url");
+        String apiKey = properties.getProperty("apiKey");
+        System.out.println(baseurl);
+
+        String url = baseurl +"/search/movie?api_key=" + apiKey + "&query=" + movieName.trim().replace(' ','+');
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println("response = " + response.body());
+
+    }
+}
