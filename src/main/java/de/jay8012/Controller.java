7d6374aa2cffd437f78c8c27deef374d193736b1
Jay8012 <sanjay.hartmann.12@gmail.com>
2024-05-11 00:13:17 +0200

    mqpping der plain api response zu einem objekt

--- src/main/java/de/jay8012/Controller.java
+++ src/main/java/de/jay8012/Controller.java
@@ -1,23 +1,36 @@
 package de.jay8012;
 
+import com.google.gson.Gson;
 import de.jay8012.exceptions.NotImplementedException;
 import de.jay8012.exceptions.UserInputException;
 import de.jay8012.models.Args;
+import de.jay8012.models.TMDBResponse;
 import de.jay8012.services.TMDbApiService;
 
 import java.io.IOException;
 
 public class Controller {
     public static void start(String[] args) throws IOException, InterruptedException {
-        Args arg;
+        Args arguments;
         try {
-            arg = new Args(args);
+            arguments = new Args(args);
         } catch (UserInputException e) {
             System.out.println(e);
             throw new NotImplementedException();
         }
 
-        new TMDbApiService().getMetadataForMovie("Star Wars 1");
-        new TMDbApiService().getMetadataForMovie("Star Wars 2");
+        if (!arguments.isFolder()) {
+
+        } else {
+            arguments.getFilePath();
+        }
+        String movie = new TMDbApiService().getMetadataForMovie("Dune: Part Two");
+        Gson gson = new Gson();
+        TMDBResponse response = gson.fromJson(movie, TMDBResponse.class);
+        System.out.println("response.getResults().length = " + response.getResults().length);
+        if (response.getTotal_results() == 1) {
+            System.out.println("Result" + response.getResults()[0]);
+        }
+//        new TMDbApiService().getMetadataForMovie("Star Wars 2");
     }
 }
