861740b8c58ee48198b281e20ec870b95099b6cc
Jay8012 <sanjay.hartmann.12@gmail.com>
2024-05-11 00:18:28 +0200

    Zeitmesser hinzugefügt

--- src/main/java/de/jay8012/Main.java
+++ src/main/java/de/jay8012/Main.java
@@ -1,11 +1,11 @@
 package de.jay8012;
 
-import de.jay8012.exceptions.NotImplementedException;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        long start = System.currentTimeMillis();
         Controller.start(args);
+        System.out.println(System.currentTimeMillis() - start);
        }
 }
