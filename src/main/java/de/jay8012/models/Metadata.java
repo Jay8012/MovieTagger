1abf99b9643bb16535089184644eb5b75b01c9bf
Jay8012 <sanjay.hartmann.12@gmail.com>
2024-05-11 00:09:48 +0200

    metadaten erstellt

---
+++ src/main/java/de/jay8012/models/Metadata.java
@@ -0,0 +1,112 @@
+package de.jay8012.models;
+
+import java.util.Arrays;
+
+public class Metadata {
+    private String poster_path;
+    private boolean adult;
+    private String overview;
+    private String release_date;
+    private int[] genre_ids;
+    private int id;
+    private String original_title;
+    private String original_language;
+    private String title;
+    private String backdrop_path;
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public int[] getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(int[] genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    @Override
+    public String toString() {
+        return "Metadata{" +
+               "poster_path='" + poster_path + '\'' +
+               ", adult=" + adult +
+               ", overview='" + overview + '\'' +
+               ", release_date='" + release_date + '\'' +
+               ", genre_ids=" + Arrays.toString(genre_ids) +
+               ", id=" + id +
+               ", original_title='" + original_title + '\'' +
+               ", original_language='" + original_language + '\'' +
+               ", title='" + title + '\'' +
+               ", backdrop_path='" + backdrop_path + '\'' +
+               '}';
+    }
+}
